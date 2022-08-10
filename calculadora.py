311a2ebb4b4a35b1a8332d4299b6de6df114ba01
Marcos Guilherme Sousa Rocha <contato.marcosguilherme@gmail.com>
2022-08-09 19:49:19 -0500

    #2 Adicionar os operadores básicos.

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,12 @@ number_1 = int(input('Enter your first number: '))
 number_2 = int(input('Enter yout second number: '))
 
 print('{} + {} = {}' .format(number_1, number_2, (number_1 + number_2)))
+
+#Subtraction
+print('{} - {} = {}' .format(number_1, number_2, (number_1 - number_2)))
+
+#Multiplication
+print('{} * {} = {}' .format(number_1, number_2, (number_1 * number_2)))
+
+#Division
+print('{} / {} = {}' .format(number_1, number_2, (number_1 / number_2)))
